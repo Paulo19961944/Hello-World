6d460583e5ea5c6757c4f15fd3ff9e12ec1ff79e
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-07-29 09:43:35 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,11 @@ package com.github.www;
 
 public class Hello {
     public static void main(String[] args) {
+
+        // Comentário
         System.out.println ("Hello World");	
+
+        /* Comentário Multi Linhas */
     
     }
 }
